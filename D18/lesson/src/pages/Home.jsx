374d50f52163c4c34b0501e89a171c8f709579f6
jbcanillo <jbcanillo@gmail.com>
2024-11-29 22:06:21 +0800

    Adde activity files for D18

---
+++ D18/lesson/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const Home = () => (
+  <div className="text-center mt-5">
+    <h1>Welcome to Movie Finder</h1>
+    <p>Search and discover your favorite movies!</p>
+  </div>
+);
+
+export default Home;
